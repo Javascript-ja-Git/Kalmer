dc2b3b427c16459b0beeb542b62283102710a19a
KalmerS <kuipalju@gmail.com>
2025-10-14 17:06:03 +0300

    tunni lõpp

---
+++ code/app.js
@@ -0,0 +1,34 @@
+//alert(`Tere maailm!`);
+
+//confirm("kas tahaksid lõunale minna?")
+
+// let firstName = "Martti";
+
+//firstName = "Miki"
+
+//alert(firstName);
+
+//let firstName = prompt("Palun sisesta oma eesnimi!");
+
+//alert(firstName);
+
+//let choise = confirm("vali ei või jah!");
+
+//alert(choise);
+
+//let choice;
+
+//alert(choice);
+
+//const number1 = prompt("sisesta esimene number")
+//const number2 = prompt("sisesta teine number")
+
+//alert(number1 - number2)
+
+//let age = 30
+//alert(age)
+//age += 1
+//alert(age)
+
+const password = prompt("sisesta parool")
+alert(password == "123");
