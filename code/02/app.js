267c9d340bf59ecb82e1c6638575bad11e23ebc0
KalmerS <kuipalju@gmail.com>
2025-10-21 13:03:51 +0300

    tempida arvutamine

--- code/02/app.js
+++ code/02/app.js
@@ -1,4 +1,39 @@
-const name = prompt(`Mis su nimi on`);
-alert(`Tere ` + name + `!`);
+//const name = prompt(`Mis su nimi on?`);
+//alert(`Tere ` + name + `!`);
+
+//const number = prompt("sisesta süniaasta")
+//alert(2025 - number)
+
+//alert(`Sinu vanus on: ` + (2025 - prompt(`Sisesta oma vanus`)))
+
+/*const year = prompt(`Sisesta oma sünniaasta`);
+const currentYear = 2025;
+const age = currentYear - year;
+alert(`Sinu vanus on: ` + age);*/
+
+// Ristküliku pindala arvutamine
+
+/*const number1 = prompt(`Sisesta ristküliku külje pikkus A`)
+const number2 = prompt(`Sisesta ristküliku küljepikkus B`)
+const pindala = number1 * number2
+alert(`Ristküliku pindala on: ` + pindala)*/
+
+/*const width = prompt(`Sisesta ristküliku laius`)
+const height = prompt(`Sisesta ristküliku kõrgus`)
+const area = width * height
+alert(`Ristküliku pindala on: ` + area)*/
+
+// Temperatuuride arvutamine
+
+/*const C = prompt(`Sisesta Celsiuse kraad`)
+const Fahrenheit = C * 9 / 5 + 32
+alert(`See on Fahrenheiti: ` + Fahrenheit)*/
+
+/*const C = prompt(`Palun sisesta Celsiuse kraadides`)
+const F = C * 9 / 5 + 32
+
+alert(`Temperatuur Farenheit skaalal: ` + F)*/
+
+
 
 
