5f8859484ea6d2747e83b61c0d15e0f04eca6af2
Kalmer Siidroo <kalmer.siidroo@opera.ee>
2025-10-28 15:29:16 +0200

    kivi-paber-käärid

--- code/03õige/app.js
+++ code/03õige/app.js
@@ -30,8 +30,94 @@ loogiliste sulgude vahel käsk, mida teha
 A on eksperiment täht mida loendada. ülemise A võib pm ära kustutada*/
 
 //Do While tsükkel
-let i=1
+/*let i=1
 do {
 console.log(i);
 i++
-} while (i<=5)
+} while (i<=5)*/
+
+//Koosta programm, mis küsib arvu ja siis väljastab arvud nullisr kuini selle arvuni
+
+/*const count = Number(prompt(`sisesta arv`))
+
+let i = 0;
+while (i <=count) {
+    console.log(i);
+    i++;
+} */
+
+//Leia käikude summa
+
+
+/*const count = Number(prompt(`sisesta arv`))
+
+let i = 0;
+let sum = 0;
+while (i <=count) {
+    sum = sum +i
+    console.log(i, sum);
+    i++;
+} */
+
+//Leia väärtus kui i ei ole 3
+
+/*const count = Number(prompt(`sisesta arv`))
+
+let i = 0;
+let sum = 0;
+while (i <=count) {
+    if (i !== 3)
+    sum = sum +i
+    console.log(i, sum);
+    i++;
+} */
+
+    //Leia vastus kui i ja summa on kokku üle 10ne
+
+  /*  *const count = Number(prompt(`sisesta arv`))
+
+let i = 0;
+let sum = 0;
+while (i <=count) {
+    if (i !== 3) {
+    sum = sum +i;
+    } 
+    if (sum > 10) {
+    console.log(i, sum);
+    } else {
+        console.log(i);
+    }
+    i++;
+}  miski asi siin ei tööta*/
+
+//KIVI-PABER-KÄÄRID
+
+/*const player1 = prompt(`Mängiga1, vali kivi-paber-käärid`)
+const player2 = prompt(`Mängija2, vali kivi-paber-käärid`)
+if (player1===player2) {
+    alert(`viik`)
+} else if (player1===`kivi` && player2 === `käärid`) {
+    alert(`Mängija 1 võitis`) 
+       } else if (player1 === `paber` && player2 === `kivi`) {
+            alert(`Mängija 1 võitis`);
+        } else if (player1 === `käärid` && player2 === `paber`) {
+            alert(`mänhija 1 võitis`)
+        } else {
+            alert(`mängija 2 võitis`)
+        }*/
+
+//KIVI-PABER-KÄÄRID 3 võiduni
+
+/*const player1 = prompt(`Mängiga1, vali kivi-paber-käärid`)
+const player2 = prompt(`Mängija2, vali kivi-paber-käärid`)
+if (player1===player2) {
+    alert(`viik`)
+} else if (player1===`kivi` && player2 === `käärid`) {
+    alert(`Mängija 1 võitis`) 
+       } else if (player1 === `paber` && player2 === `kivi`) {
+            alert(`Mängija 1 võitis`);
+        } else if (player1 === `käärid` && player2 === `paber`) {
+            alert(`mänhija 1 võitis`)
+        } else {
+            alert(`mängija 2 võitis`)
+        }*/
